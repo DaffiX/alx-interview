88aa7763f64963e43c82dd87eec8530345f7be79
DaffiX <suleydaffy@gmail.com>
2023-07-13 20:35:30 +0300

    Change Solution

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -3,16 +3,13 @@
     A function that returns a list of list of intergers representing the Pascal Triangle
 '''
 def pascal_triangle(n):
-    #note n is the number of row
-    if n <= 0:
-        return []
-    
-    triangle = [[1]]
-    for i in range(1, n):
-        row = [1]
-        for j in range(1, i):
-            row.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
-        row.append(1)
-        triangle.append(row)
-
-    return triangle
+    res = []
+    if n > 0:
+        for i in range(n):
+            level = []
+            C = 1
+            for j in range(i + 1):
+                level.append(C)
+                C = C * (i - j) // (j + 1)
+            res.append(level)
+    return res
