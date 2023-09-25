bd646294082fad070c52b2fe91592ea24faac5ff
DaffiX <suleydaffy@gmail.com>
2023-09-25 19:06:39 +0300

    updated soln

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,21 +1,18 @@
 #!/usr/bin/python3
-"""Function that takes coins and combine min number to get total """
+"""
+Change comes from within
+"""
 
 
 def makeChange(coins, total):
-
+    """
+    Given a pile of coins of different values,
+    determine the fewest number of coins needed to meet a given amount total
+    """
     if total <= 0:
         return 0
-
-    dp = [float('inf')] * (total + 1)
-    dp[0] = 0
-
-    for i in range(1, total + 1):
-        for coin in coins:
-            if i - coin >= 0:
-                dp[i] = min(dp[i], dp[i - coin] + 1)
-
-    if dp[total] == float('inf'):
-        return -1
-    else:
-        return dp[total]
+    dp = [0] + [float("inf")] * (total)
+    for coin in coins:
+        for i in range(coin, total + 1):
+            dp[i] = min(dp[i], dp[i - coin] + 1)
+    return dp[-1] if dp[-1] != float("inf") else -1
