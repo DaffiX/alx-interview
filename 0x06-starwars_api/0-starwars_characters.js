801aef0401d2d9e21fcae692dda73eb10bdf508f
DaffiX <suleydaffy@gmail.com>
2023-09-25 10:36:32 +0300

    added newline

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -2,25 +2,26 @@
 
 const request = require('request');
 
-const base_url = 'https://swapi-api.hbtn.io/api';
-
-const fetchCharacters = (actors, index) => {
-  if (index === actors.length) return;
-
-  const characterUrl = actors[index];
-  request(characterUrl, (err, res, body) => {
-    if (err) throw err;
-
-    const characterData = JSON.parse(body);
-    console.log(characterData.name);
-
-    fetchCharacters(actors, index + 1);
+const req = (arr, i) => {
+  if (i === arr.length) return;
+  request(arr[i], (err, response, body) => {
+    if (err) {
+      throw err;
+    } else {
+      console.log(JSON.parse(body).name);
+      req(arr, i + 1);
+    }
   });
 };
 
-request(`${base_url}/films/${process.argv[2]}`, (err, res, body) => {
-  if (err) throw err;
-
-  const actors = JSON.parse(body).characters;
-  fetchCharacters(actors, 0);
-});
+request(
+  `https://swapi-api.hbtn.io/api/films/${process.argv[2]}`,
+  (err, response, body) => {
+    if (err) {
+      throw err;
+    } else {
+      const chars = JSON.parse(body).characters;
+      req(chars, 0);
+    }
+  }
+);
